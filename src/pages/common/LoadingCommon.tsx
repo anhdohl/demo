33f88a7de84c5eee5f9231ee0ff835be4bce18b8
anhdohl <anh.do@consultinghnl.com>
2024-09-10 11:13:30 +0700

    fix

--- src/pages/common/LoadingCommon.tsx
+++ src/pages/common/LoadingCommon.tsx
@@ -7,7 +7,7 @@ export default function LoadingCommon() {
             <CommandDialog open={true}>
                 <div className='h-[239px] flex justify-center content-center bg-[#ffffff]'>
                     <div className='h-[100%] flex justify-center content-center flex-col'>
-                        <div className='text-[#000000] text-[25px] text-center'>Creating a table...</div>
+                        <div className='text-[#000000] text-[25px] text-center'>Creating a ta222ble...</div>
                         <div className='text-[#000000] text-[16px] text-center'>It may take a couple of moments</div>
                     </div>
                 </div>
